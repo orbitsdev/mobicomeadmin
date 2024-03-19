bc7adbb051369c558082ef8f444f61ac92d83e46
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-03-20 02:14:16 +0800

    wip

--- app/Http/Resources/TakedExamResource.php
+++ app/Http/Resources/TakedExamResource.php
@@ -23,33 +23,34 @@ class TakedExamResource extends JsonResource
             'total_questions' => $this->getTotalExerciseQuestions(),
             'total_score' => $this->getRealScore(),
             'total_mistake' => $this->getTotalWrongAnswer(),
-            'answers' => $this->answers->map(function ($answer) {
-                if ($this->excercise->type === "Multiple Choice") {
-                    return [
-                        'id' => $this->id,
-                        'excercise_id' => $this->excercise_id,
-                        'question' => $this->question->question,
-                        'correct_answer' => $this->question->multiple_choice->correct_answer,
-                        'options' => $this->question->multiple_choice->getShuffledOptionsAttribute(),
-                    ];
-                } elseif ($this->excercise->type === "True or False") {
-                    return [
-                        'id' => $this->id,
-                        'excercise_id' => $this->excercise_id,
-                        'question' => $this->question->question,
-                        'correct_answer' => $this->question->true_or_false->correct_answer,
-                    ];
-                } elseif ($this->excercise->type === "Fill in the Blank") {
-                    return [
-                        'id' => $this->id,
-                        'excercise_id' => $this->excercise_id,
-                        'question' => $this->question->question,
-                        'correct_answer' => $this->question->fill_in_the_blank->correct_answer,
-                    ];
-                } else {
-                    return [];
-                }
-            }),
+            'answers' => $this->answers,
+            // 'answers' => $this->answers->map(function ($answer) {
+            //     if ($this->excercise->type === "Multiple Choice") {
+            //         return [
+            //             'id' => $this->id,
+            //             'excercise_id' => $this->excercise_id,
+            //             'question' => $this->question->question,
+            //             'correct_answer' => $this->question->multiple_choice->correct_answer,
+            //             'options' => $this->question->multiple_choice->getShuffledOptionsAttribute(),
+            //         ];
+            //     } elseif ($this->excercise->type === "True or False") {
+            //         return [
+            //             'id' => $this->id,
+            //             'excercise_id' => $this->excercise_id,
+            //             'question' => $this->question->question,
+            //             'correct_answer' => $this->question->true_or_false->correct_answer,
+            //         ];
+            //     } elseif ($this->excercise->type === "Fill in the Blank") {
+            //         return [
+            //             'id' => $this->id,
+            //             'excercise_id' => $this->excercise_id,
+            //             'question' => $this->question->question,
+            //             'correct_answer' => $this->question->fill_in_the_blank->correct_answer,
+            //         ];
+            //     } else {
+            //         return [];
+            //     }
+            // }),
             'created_at' => Carbon::parse($this->created_at)->format('F j, Y g:i A'),
             'updated_at' => Carbon::parse($this->updated_at)->format('F j, Y g:i A'),
         ];
