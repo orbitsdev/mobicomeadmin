834b88527845bb259b77b4564298527bf5213b9e
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-03-21 23:49:53 +0800

    wip

--- resources/views/infolists/components/student-exercises-details.blade.php
+++ resources/views/infolists/components/student-exercises-details.blade.php
@@ -1,39 +1,40 @@
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
     <div>
+        <div class="grid grid-cols-12 border-b">
+            <div class="col-span-8">
+                <h2 class="text-lg font-semibold mb-2">Exam Records</h2>
+            </div>
+            <div class="col-span-2">
+                <h2 class="text-lg font-semibold mb-2">Score</h2>
+            </div>
+            <div class="col-span-2">
+                <h2 class="text-lg font-semibold mb-2">Date</h2>
+            </div>
+        </div>
 
         @foreach ($getRecord()->taked_exams as $taked_exam)
             <div class="grid grid-cols-12 border-b">
                 <div class="col-span-8">
                     <a href="{{ route('teacher-view-exercise-score', ['record' => $taked_exam]) }}"
-                        class="p-2 text-blue-600 block capitalize"
-                        style="color: #3b82f6">{{ $taked_exam->excercise->title }}</a>
+                        class="p-2 text-blue-600 block capitalize hover:underline">{{ $taked_exam->excercise->title }}</a>
                 </div>
                 <div class="col-span-2">
-                    <p>Total Score: {{ $taked_exam->getRealScore() }} / {{ $taked_exam->getTotalExerciseQuestions() }}
-                    </p>
+                    <p>Total Score: <span class="text-green-600">{{ $taked_exam->getRealScore() }}</span> / {{ $taked_exam->getTotalExerciseQuestions() }}</p>
                 </div>
                 <div class="col-span-2">
                     <p>{{ Carbon\Carbon::parse($taked_exam->created_at)->format('F j, Y') }}</p>
                 </div>
             </div>
             @if ($taked_exam->feed)
-                <div class="bg-gray-50  rounded-lg p-2 text-xs">
-                    <h2 class=" ">Feedback:</h2>
-                    <div class="">
-                        <p class="col-span-1 text-sm text-gray-600"><span class="">Rate:</span>
-                            {{ $taked_exam->feed->rate }}</p>
-                        <p class="col-span-1 text-sm text-gray-600 "><span class="">Message:</span>
-                            {{ $taked_exam->feed->message }}</p>
-                        <p class="col-span-1 text-sm text-gray-600"><span class="">Is Read:</span>
-                            {{ $taked_exam->feed->is_read ? 'Yes' : 'No' }}</p>
-
+                <div class="bg-gray-100 rounded-lg p-2 text-xs mt-2">
+                    <h2 class="text-lg font-semibold mb-2">Feedback:</h2>
+                    <div class="grid grid-cols-1">
+                        <p><span class="font-semibold">Rate:</span> {{ $taked_exam->feed->rate }}</p>
+                        <p><span class="font-semibold">Message:</span> {{ $taked_exam->feed->message }}</p>
+                        <p><span class="font-semibold">Is Read:</span> {{ $taked_exam->feed->is_read ? 'Yes' : 'No' }}</p>
                     </div>
                 </div>
             @endif
         @endforeach
     </div>
-
-
-
-
 </x-dynamic-component>
