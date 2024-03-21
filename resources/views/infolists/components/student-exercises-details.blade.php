a00541b4fba959e1642e91e7c53ee24cafbe316c
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-03-21 23:53:40 +0800

    wip

--- resources/views/infolists/components/student-exercises-details.blade.php
+++ resources/views/infolists/components/student-exercises-details.blade.php
@@ -17,7 +17,7 @@
 
         @foreach ($getRecord()->taked_exams as $taked_exam)
             <div class="grid grid-cols-12 border-b">
-                <div class="col-span-8">
+                <div class="col-span-6">
                     <a href="{{ route('teacher-view-exercise-score', ['record' => $taked_exam]) }}"
                         class="p-2 text-blue-600 block capitalize hover:underline">{{ $taked_exam->excercise->title }}</a>
                 </div>
