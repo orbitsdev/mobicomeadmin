8a8e7547b4af03f27eca92ce15168c21117778b8
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-03-22 16:42:50 +0800

    wip

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -31,8 +31,8 @@
             <div class="mt-4">
                 <x-label for="role" value="{{ __('Register As') }}" />
                 <select id="role" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  name="role" :value="old('role')">
-                    <option value="Admin">Admin</option>
                     <option value="Teacher">Teacher</option>
+                    <option value="Admin">Admin</option>
 
                   </select>
 
