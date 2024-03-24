8aadcf2d8c3145e201fbc4e6a8dd70c848cef851
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-03-24 14:26:59 +0800

    wip

--- app/Livewire/Teachers/ListTeachers.php
+++ app/Livewire/Teachers/ListTeachers.php
@@ -162,9 +162,11 @@ class ListTeachers extends Component implements HasForms, HasTable
                                     name: 'sections',
                                     titleAttribute: 'title'
                                 )
-                                ->createForm([
+                                ->createOptionForm([
                                     TextInput::make('title'),
+                                  
                                 ])
+                                
                                 ->getOptionLabelFromRecordUsing(fn (Model $record) => "{$record->title}")
                                 ->bulkToggleable()
                                 ->columns(3)
