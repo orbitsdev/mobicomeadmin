48705cf3605b57d74e4baf233b43f4275cbc697d
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-04-06 16:20:47 +0800

    wip

--- app/Livewire/Exercise/ListTakedExam.php
+++ app/Livewire/Exercise/ListTakedExam.php
@@ -59,7 +59,7 @@ class ListTakedExam extends Component implements HasForms, HasTable
                     ->label('Actions'),
             ])
             ->groups([
-                Group::make('student.enrolled_section.created_at')
+                Group::make('student.enrolled_section.section.title')
                     ->label('Section')
                     // ->titlePrefixedWithLabel(false)
                     ,
