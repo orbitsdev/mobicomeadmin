059b1d734b68e7d4345e805364fd5192fd07af99
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-04-06 17:40:57 +0800

    wip

--- app/Livewire/Exercise/ListTeacherUnderExercise.php
+++ app/Livewire/Exercise/ListTeacherUnderExercise.php
@@ -9,8 +9,12 @@ use App\Models\Excercise;
 use Filament\Tables\Table;
 use Illuminate\Contracts\View\View;
 use Filament\Forms\Contracts\HasForms;
+use Filament\Tables\Actions\BulkAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Actions\DeleteAction;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Tables\Concerns\InteractsWithTable;
 
@@ -26,30 +30,33 @@ class ListTeacherUnderExercise extends Component implements HasForms, HasTable
         return $table
             ->query(Teacher::query())
             ->columns([
-                Tables\Columns\TextColumn::make('user_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\IconColumn::make('is_approved')
-                    ->boolean(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('user')->formatStateUsing(function ($state) {
+                    return $state->getFullName();
+                })
+                    ->label('Name')
+                    ->searchable(query: function (Builder $query, string $search): Builder {
+                        return $query->whereHas('user', function ($query) use ($search) {
+                            $query->where('first_name', 'like', "%{$search}%")
+                                ->orWhere('last_name', 'like', "%{$search}%");
+                        });
+                    }),
+
+                    TextColumn::make('user.email')->label('Email')->searchable(),
             ])
             ->filters([
                 //
             ])
             ->actions([
-                //
+                DeleteAction::make('delete'),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    //
-                ]),
+
+                    BulkAction::make('delete')
+                        ->requiresConfirmation()
+                        ->action(fn (Collection $records) => $records->each->delete())
+                ])
+                    ->label('Actions'),
             ]);
     }
 
