c5e2ba09bfff97bd624c0b0453a1df39e35ab2b2
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-04-07 17:55:22 +0800

    update link back

--- resources/views/livewire/exercise/list-taked-exam.blade.php
+++ resources/views/livewire/exercise/list-taked-exam.blade.php
@@ -1,8 +1,11 @@
 <div>
     <div class="bg-white rounded-md shadow-md p-6 mb-6">
-        <h1 class="text-2xl uppercase font-bold mb-4">
-            {{ $record->title }} ({{ $record->type }} ) - {{ $record->getTotalQuestions() }} Questions
-        </h1>
+        <div class="flex justify-between items-center mb-4">
+            <h1 class="text-2xl uppercase font-bold">
+                {{ $record->title }} ({{ $record->type }}) - {{ $record->getTotalQuestions() }} Questions
+            </h1>
+            <a href="{{ route('list-excercises') }}" class="text-blue-500 hover:text-blue-700">Back</a>
+        </div>
         
         <div class="border-t prose max-w-none py-2">
             @markdown($record->description)
